4497ea6667a70c208485964adf88215d6fc14cd1
Dfreequark <161359772+Dfreequark@users.noreply.github.com>
2024-10-01 10:39:14 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -11,6 +11,21 @@ logger = get_logger('Langchain-Chatbot')
 os.environ["LANGCHAIN_API_KEY"]= st.secrets["LANGCHAIN_API_KEY"]
 os.environ["LANGCHAIN_TRACING_V2"]="false"
 
+
+def user_api_key():
+    cohere_api_key = st.sidebar.text_input(
+        label="Enter Your Cohere API Key",
+        type="password",
+        placeholder="api key...",
+        key="SELECTED_COHERE_API_KEY"
+        )
+    if not cohere_api_key:
+        st.error("Please add your Cohere API key to continue.")
+        st.info("Obtain your key from this link: https://cohere.com/")
+        st.stop()
+
+    return cohere_api_key
+
 #decorator
 def enable_chat_history(func):
     # if os.environ.get("OPENAI_API_KEY"):
@@ -53,20 +68,6 @@ def clear_chat_history():
 def display_msg(msg, author):
     st.session_state.messages.append({"role": author, "content": msg})
     st.chat_message(author).write(msg)
-
-def user_api_key():
-    cohere_api_key = st.sidebar.text_input(
-        label="Enter Your Cohere API Key",
-        type="password",
-        placeholder="api key...",
-        key="SELECTED_COHERE_API_KEY"
-        )
-    if not cohere_api_key:
-        st.error("Please add your Cohere API key to continue.")
-        st.info("Obtain your key from this link: https://cohere.com/")
-        st.stop()
-
-    return cohere_api_key
 
 
 def configure_llm():
